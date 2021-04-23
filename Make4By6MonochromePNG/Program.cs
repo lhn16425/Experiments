18324ee32c4205c169e798623e493c8210a45e38
Long Nguyen <lnguyen@stamps.com>
2021-04-23 09:22:13 -0700

    Initial check-in.

---
+++ Make4By6MonochromePNG/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Make4By6MonochromePNG
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Util.Make4By6MonochromePNG("D:\\Downloads\\label-200.png", "D:\\Downloads\\label4x6-200.png");
+
+			using (FileStream ofs = File.OpenWrite("D:\\Downloads\\label4x6-200.png"))
+			{
+				byte[] imageData = File.ReadAllBytes("D:\\Downloads\\label-200.png");
+				string img = Util.Get4x6MonochromePNGImage(imageData);
+				imageData = Convert.FromBase64String(img);
+				ofs.Write(imageData, 0, imageData.Length);
+			}
+		}
+	}
+}
